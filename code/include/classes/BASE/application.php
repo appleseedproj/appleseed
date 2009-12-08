6f293af3ba0fb0e5d44b3da6c8af7666bbe847ce
Michael Chisari <michael.chisari@gmail.com>
2009-12-08 05:15:26 +0000

    Updated CheckEmail function to use preg_match, more standards-compliant.

--- code/include/classes/BASE/application.php
+++ code/include/classes/BASE/application.php
@@ -822,47 +822,52 @@
     } // ConvertType
 
     // Check for a valid email address 
-    function CheckEmail ($pEMAIL) {
-      // Adopted from ilovjackdaniels.com sample code.
-      // Original Author: dave@ilovejackdaniels.com
-  
-      // Remove trailing white space.
-      $pEMAIL = ltrim (rtrim ($pEMAIL));
-  
-      // Check that there's one @ symbol, and that the lengths are right
-      if (!preg_match("[^@]{1,64}@[^@]{1,255}", $pEMAIL)) {
-        // Email invalid because wrong number of characters in one section, or wrong number of @ symbols.
-        return false;
-      } // if
-  
-      // Split it into sections to make life easier
-      $email_array = explode("@", $pEMAIL);
-      $local_array = explode(".", $email_array[0]);
-  
-      for ($i = 0; $i < sizeof($local_array); $i++) {
-         if (!preg_match("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])) {
-          return false;
+    function CheckEmail ($email) {
+    	// Adapted From: http://www.linuxjournal.com/article/9585
+        $isValid = true;
+        $atIndex = strrpos($email, "@");
+        
+        if (is_bool($atIndex) && !$atIndex) {
+            $isValid = false;
+        } else {
+            $domain = substr($email, $atIndex+1);
+            $local = substr($email, 0, $atIndex);
+            $localLen = strlen($local);
+            $domainLen = strlen($domain);
+            if ($localLen < 1 || $localLen > 64) {
+                // local part length exceeded
+                $isValid = false;
+            } else if ($domainLen < 1 || $domainLen > 255) {
+        	    // domain part length exceeded
+    	    	$isValid = false;
+    	    } else if ($local[0] == '.' || $local[$localLen-1] == '.') {
+    	        // local part starts or ends with '.'
+                $isValid = false;
+            } else if (preg_match('/\\.\\./', $local)) {
+                // local part has two consecutive dots
+                $isValid = false;
+            } else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
+                // character not valid in domain part
+                $isValid = false;
+            } else if (preg_match('/\\.\\./', $domain)) {
+                // domain part has two consecutive dots
+                $isValid = false;
+            } else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/', str_replace("\\\\","",$local))) {
+                // character not valid in local part unless 
+                // local part is quoted
+                if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\","",$local))) {
+                    $isValid = false;
+                } // if
+            } // if
+            if ($isValid && !(checkdnsrr($domain,"MX") ||  checkdnsrr($domain,"A"))) {
+                // domain not found in DNS
+                // NOTE: Removed Temporarily
+                // $isValid = false;
+            } // if
         } // if
-      } // if
-  
-      // Check if domain is IP. If not, it should be valid domain name
-      if (!preg_match("^\[?[0-9\.]+\]?$", $email_array[1])) { 
-        $domain_array = explode(".", $email_array[1]);
-  
-        if (sizeof($domain_array) < 2) {
-            // Not enough parts to domain
-            return false; 
-        } // if
-  
-        for ($i = 0; $i < sizeof($domain_array); $i++) {
-          if (!preg_match("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
-            return false;
-          } // if
-        } // for
-      } // if
-  
-      return TRUE;
+        return $isValid;
     } // CheckEmail
+    
 
     // Remove the extension from a filename.
     function RemoveExtension ($pFILENAME) { 
