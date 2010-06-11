aa9e3f9b8de1796c3d1d4b207ac06ca0febd1bab
Michael Chisari <michael.chisari@gmail.com>
2010-06-11 04:26:55 +0000

    Added proper comment header to nicedit component entry file.

--- components/nicedit/nicedit.php
+++ components/nicedit/nicedit.php
@@ -1,8 +1,12 @@
 <?php
-/*
- * Created on Jun 10, 2010
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
+/**
+ * @version      $Id$
+ * @package      Appleseed.Component
+ * @subpackage   Nicedit
+ * @copyright    Copyright (C) 2004 - 2010 Michael Chisari. All rights reserved.
+ * @link         http://opensource.appleseedproject.org
+ * @license      GNU General Public License version 2.0 (See LICENSE.txt)
  */
-?>
+
+// Restrict direct access
+defined( 'APPLESEED' ) or die( 'Direct Access Denied' );
