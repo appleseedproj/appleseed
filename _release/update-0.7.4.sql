2a2493f0f90850a2249c961839c1d0749cd2c941
Michael Chisari <michael.chisari@gmail.com>
2010-06-11 00:41:12 +0000

    Added update sql script for release, and created beginnings of nicedit editor component+hook

---
+++ _release/update-0.7.4.sql
@@ -0,0 +1,2 @@
+update #__userSettings set Value='default' where Identifier='DefaultTheme';
+update #__systemConfig set Value='default' where Concern='Theme';
---
+++ hooks/editor/nicedit/nicedit.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Created on Jun 10, 2010
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
