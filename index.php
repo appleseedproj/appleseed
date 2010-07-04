fd198f48f5080ec3a30b17df38ef5a97db890928
Michael Chisari <michael.chisari@gmail.com>
2010-07-04 05:20:53 +0000

    Changed "success" view to use the new theme layout.

--- index.php
+++ index.php
@@ -141,8 +141,10 @@ header, section, footer { padding: 10px 0; }
 
 time { font-size: 85%; }
 
-.yes { padding:1px 2px; color:#ffffff; border:1px solid #007700; background-color:#009900; }
+span.done, .yes { padding:1px 2px; color:#ffffff; border:1px solid #007700; background-color:#009900; }
 .no { padding:1px 2px; color:#ffffff; border:1px solid #770000; background-color:#990000; }
+
+p.error { width:96%; clear:both; padding:3px 2%; margin:2px 0; background-color:#639a00; background-color:#bf4630; color:#f1ffd6; }
 
 footer ul li { list-style: none; }
 .container_12,.container_16{margin-left:auto;margin-right:auto;width:960px}.grid_1,.grid_2,.grid_3,.grid_4,.grid_5,.grid_6,.grid_7,.grid_8,.grid_9,.grid_10,.grid_11,.grid_12,.grid_13,.grid_14,.grid_15,.grid_16{display:inline;float:left;margin-left:10px;margin-right:10px}.push_1,.pull_1,.push_2,.pull_2,.push_3,.pull_3,.push_4,.pull_4,.push_5,.pull_5,.push_6,.pull_6,.push_7,.pull_7,.push_8,.pull_8,.push_9,.pull_9,.push_10,.pull_10,.push_11,.pull_11,.push_12,.pull_12,.push_13,.pull_13,.push_14,.pull_14,.push_15,.pull_15{position:relative}.container_12 .grid_3,.container_16 .grid_4{width:220px}.container_12 .grid_6,.container_16 .grid_8{width:460px}.container_12 .grid_9,.container_16 .grid_12{width:700px}.container_12 .grid_12,.container_16 .grid_16{width:940px}.alpha{margin-left:0}.omega{margin-right:0}.container_12 .grid_1{width:60px}.container_12 .grid_2{width:140px}.container_12 .grid_4{width:300px}.container_12 .grid_5{width:380px}.container_12 .grid_7{width:540px}.container_12 .grid_8{width:620px}.container_12 .grid_10{width:780px}.container_12 .grid_11{width:860px}.container_16 .grid_1{width:40px}.container_16 .grid_2{width:100px}.container_16 .grid_3{width:160px}.container_16 .grid_5{width:280px}.container_16 .grid_6{width:340px}.container_16 .grid_7{width:400px}.container_16 .grid_9{width:520px}.container_16 .grid_10{width:580px}.container_16 .grid_11{width:640px}.container_16 .grid_13{width:760px}.container_16 .grid_14{width:820px}.container_16 .grid_15{width:880px}.container_12 .prefix_3,.container_16 .prefix_4{padding-left:240px}.container_12 .prefix_6,.container_16 .prefix_8{padding-left:480px}.container_12 .prefix_9,.container_16 .prefix_12{padding-left:720px}.container_12 .prefix_1{padding-left:80px}.container_12 .prefix_2{padding-left:160px}.container_12 .prefix_4{padding-left:320px}.container_12 .prefix_5{padding-left:400px}.container_12 .prefix_7{padding-left:560px}.container_12 .prefix_8{padding-left:640px}.container_12 .prefix_10{padding-left:800px}.container_12 .prefix_11{padding-left:880px}.container_16 .prefix_1{padding-left:60px}.container_16 .prefix_2{padding-left:120px}.container_16 .prefix_3{padding-left:180px}.container_16 .prefix_5{padding-left:300px}.container_16 .prefix_6{padding-left:360px}.container_16 .prefix_7{padding-left:420px}.container_16 .prefix_9{padding-left:540px}.container_16 .prefix_10{padding-left:600px}.container_16 .prefix_11{padding-left:660px}.container_16 .prefix_13{padding-left:780px}.container_16 .prefix_14{padding-left:840px}.container_16 .prefix_15{padding-left:900px}.container_12 .suffix_3,.container_16 .suffix_4{padding-right:240px}.container_12 .suffix_6,.container_16 .suffix_8{padding-right:480px}.container_12 .suffix_9,.container_16 .suffix_12{padding-right:720px}.container_12 .suffix_1{padding-right:80px}.container_12 .suffix_2{padding-right:160px}.container_12 .suffix_4{padding-right:320px}.container_12 .suffix_5{padding-right:400px}.container_12 .suffix_7{padding-right:560px}.container_12 .suffix_8{padding-right:640px}.container_12 .suffix_10{padding-right:800px}.container_12 .suffix_11{padding-right:880px}.container_16 .suffix_1{padding-right:60px}.container_16 .suffix_2{padding-right:120px}.container_16 .suffix_3{padding-right:180px}.container_16 .suffix_5{padding-right:300px}.container_16 .suffix_6{padding-right:360px}.container_16 .suffix_7{padding-right:420px}.container_16 .suffix_9{padding-right:540px}.container_16 .suffix_10{padding-right:600px}.container_16 .suffix_11{padding-right:660px}.container_16 .suffix_13{padding-right:780px}.container_16 .suffix_14{padding-right:840px}.container_16 .suffix_15{padding-right:900px}.container_12 .push_3,.container_16 .push_4{left:240px}.container_12 .push_6,.container_16 .push_8{left:480px}.container_12 .push_9,.container_16 .push_12{left:720px}.container_12 .push_1{left:80px}.container_12 .push_2{left:160px}.container_12 .push_4{left:320px}.container_12 .push_5{left:400px}.container_12 .push_7{left:560px}.container_12 .push_8{left:640px}.container_12 .push_10{left:800px}.container_12 .push_11{left:880px}.container_16 .push_1{left:60px}.container_16 .push_2{left:120px}.container_16 .push_3{left:180px}.container_16 .push_5{left:300px}.container_16 .push_6{left:360px}.container_16 .push_7{left:420px}.container_16 .push_9{left:540px}.container_16 .push_10{left:600px}.container_16 .push_11{left:660px}.container_16 .push_13{left:780px}.container_16 .push_14{left:840px}.container_16 .push_15{left:900px}.container_12 .pull_3,.container_16 .pull_4{left:-240px}.container_12 .pull_6,.container_16 .pull_8{left:-480px}.container_12 .pull_9,.container_16 .pull_12{left:-720px}.container_12 .pull_1{left:-80px}.container_12 .pull_2{left:-160px}.container_12 .pull_4{left:-320px}.container_12 .pull_5{left:-400px}.container_12 .pull_7{left:-560px}.container_12 .pull_8{left:-640px}.container_12 .pull_10{left:-800px}.container_12 .pull_11{left:-880px}.container_16 .pull_1{left:-60px}.container_16 .pull_2{left:-120px}.container_16 .pull_3{left:-180px}.container_16 .pull_5{left:-300px}.container_16 .pull_6{left:-360px}.container_16 .pull_7{left:-420px}.container_16 .pull_9{left:-540px}.container_16 .pull_10{left:-600px}.container_16 .pull_11{left:-660px}.container_16 .pull_13{left:-780px}.container_16 .pull_14{left:-840px}.container_16 .pull_15{left:-900px}.clear{clear:both;display:block;overflow:hidden;visibility:hidden;width:0;height:0}.clearfix:after{clear:both;content:' ';display:block;font-size:0;line-height:0;visibility:hidden;width:0;height:0}* html .clearfix,*:first-child+html .clearfix{zoom:1}
@@ -432,7 +434,7 @@ textarea { min-width:500px; min-height:160px; }
 	} // resetCheck
 </script>
 
-<?php if ($CurrentStep == 1) $INSTALL->ViewStepOne (); ?>
+<?php if ($CurrentStep == 1) $INSTALL->ViewStepTwo (); ?>
 <?php if ($CurrentStep == 2) $INSTALL->ViewStepTwo (); ?>
 
 <?php
@@ -1063,6 +1065,10 @@ class cINSTALL {
 								<li>Lots of bug fixes</li>
 							</ul>
 
+						<h3>Appleseed Project Homepage</h3>
+						
+						&rarr; <a href="http://opensource.appleseedproject.org">opensource.appleseedproject.org</a>
+
 					</section>
 				</div>
         <div id="page_right" class="grid_9">
@@ -1070,7 +1076,10 @@ class cINSTALL {
   	      <section id="install">
             <h1>Appleseed Install v0.7.4</h1>
       
-            <?php echo $ErrorString; ?>
+       			<?php if ($ErrorString) { 
+							echo "<p class='error'>$ErrorString</p>";
+							}
+					  ?>
             <form id='main' name='main' method='POST' action='/'>
               <fieldset id='check'>
                 <legend>System Check</legend>
@@ -1112,6 +1121,12 @@ class cINSTALL {
                 			<th><span class='label'>Mysql version 5.0 or higher? (Client is <?php echo $this->GetMysqlClientVersion (); ?>)</span></th>
                 			<td><?php echo $ErrorMark['mysql_client_version']; ?></td>
 										</tr>
+
+									  <tr>
+											<th><input type='submit' name='refresh' class='refresh' value="Refresh" /></th>
+										  <td></td>
+										</tr>
+     
 									</tbody>
 								</table>
       
@@ -1123,11 +1138,6 @@ class cINSTALL {
 
 								<table>
 								  <tbody>
-									  <tr>
-										  <th></th>
-											<td><input type='submit' name='refresh' class='refresh' value="Refresh" /></td>
-										</tr>
-     
 										<tr>
                 			<th><label for='gHOST'>DB Host Name:</label></th>
                 			<td><input type='text' class='gHOST' name='gHOST' value='<?php echo $gHOST; ?>' /></td>
@@ -1206,6 +1216,9 @@ class cINSTALL {
             </form>
 	       </section>
          </div>
+	     	 <div class="clear"></div>
+			 </div>
+
        <footer class='container_12'>
 			  	<p align="center">
         		Copyright &copy; 2004-2010 by Michael Chisari under the <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU GPL Version 2</a>. All Rights Reserved.
@@ -1227,74 +1240,154 @@ class cINSTALL {
     ?>
         
     <body>
-     <div id='install'>
-      <div class='caption'>Appleseed Install v0.7.4</div>
-      <div class='container'>
+      <div id="page" class="container_12">
+        <div id="page_left" class="grid_3"> 
+					<section id="info">
+						<h1>Welcome to Appleseed!</h1> 
+						<h2>Open Source + Distributed Social Networking</h2>
+
+						<p> Appleseed is the first open source, fully decentralized social networking software. </p>
+							<ul>
+							  <li>Protect your privacy</li>
+							  <li>Move around without losing friends</li>
+							  <li>Support open standards</li>
+							</ul>
+
+						<h3>Appleseed Features</h3>
+							<ul>
+								<li>Connect with Friends!</li>
+								<li>Blogging</li>
+								<li>Journals</li>
+								<li>Photos</li>
+								<li>Spam-Resistant E-Mail</li>
+								<li>Decentralized</li>
+							</ul>
+						<h3>New in this release</h3>
+							<ul>
+								<li>Themes (and a new look!)</li>
+								<li>MVC+Plugins Framework</li>
+								<li>Internationalization</li>
+								<li>Stronger Password Encryption</li>
+								<li>Lots of bug fixes</li>
+							</ul>
+
+						<h3>Appleseed Project Homepage</h3>
+						
+						&rarr; <a href="http://opensource.appleseedproject.org">opensource.appleseedproject.org</a>
+
+					</section>
+				</div>
+        <div id="page_right" class="grid_9">
+										         
+  	      <section id="install">
+            <h1>Appleseed Install v0.7.4</h1>
       
-       <?php echo $ErrorString; ?>
-       <div id='check'>
-        <p class='title'>System Check</p>
-        <p class='information'>This system cannot be installed until the following conditions are met.</p>
-        
-        <span class='label'>Installed in root directory? (see documentation)</span>
-        <?php echo $ErrorMark['sub_dir']; ?>
-        
-        <span class='label'>Is register_globals turned off?</span>
-        <?php echo $ErrorMark['register_globals']; ?>
-        
-        <span class='label'>Is the photos/ directory writable?</span>
-        <?php echo $ErrorMark['photo_directory']; ?>
-        
-        <span class='label'>Is the _storage/ directory writable?</span>
-        <?php echo $ErrorMark['attachment_directory']; ?>
-        
-        <span class='label'>Is the configurations/ directory writable?</span>
-        <?php echo $ErrorMark['configurations_directory']; ?>
-        
-        <span class='label'>Is .htaccess writable?</span>
-        <?php echo $ErrorMark['htaccess_final']; ?>
-        
-        <span class='label'>PHP version 5.2 or higher? (Running <?php echo phpversion(); ?>)</span>
-        <?php echo $ErrorMark['php_version']; ?>
-        
-        <span class='label'>Mysql version 5.0 or higher? (Running <?php echo $this->GetMysqlClientVersion (); ?>)</span>
-        <?php echo $ErrorMark['mysql_client_version']; ?>
-       </div> <!-- #check -->
+       			<?php if ($ErrorString) { 
+								echo "<p class='error'>$ErrorString</p>";
+							}
+					  ?>
+            <form id='main' name='main' method='POST' action='/'>
+              <fieldset id='check'>
+                <legend>System Check</legend>
        
-      <div id='completed'>
-        <p class='title'>Completing Installation</p>
-        <p class='information'>Creating database tables and initializing Appleseed.</p>
-        
-        <span class='label'>Writing site data to file</span>
-        <span class='done'>DONE</span>
-        
-        <span class='label'>Creating .htaccess file</span>
-        <span class='done'>DONE</span>
-        
-        <span class='label'>Creating Appleseed Tables</span>
-        <span class='done'>DONE</span>
-        
-        <span class='label'>Importing Initial Data</span>
-        <span class='done'>DONE</span>
-        
-        <span class='label'>Creating Administrator Account</span>
-        <span class='done'>DONE</span>
-        
-        <p class='done'>
-        Your Appleseed installation is now complete!  Be sure to 
-        change the permissions on <b>.htaccess</b> and <b>site.adat</b> 
-        so that they aren't writable anymore, and delete the install
-        script (index.php) from the appleseed root directory. </p>
-        
-        <p class='final'><a href='<?php echo $gDOMAIN; ?>/login/'>Click Here To Login To Your Appleseed Site</a></p>
-        
-      </div> <!-- #completed -->
-      </div> <!-- .container -->
+								<table>
+									<tbody>
+										<tr>
+                			<th><span class='label'>Installed in site root directory? (see documentation)</span></th>
+                			<td><?php echo $ErrorMark['sub_dir']; ?></td>
+										</tr>
       
-     </div> <!-- .install -->
-     <div id='copyright'>
-      Copyleft &copy; 2004-2008 by the Appleseed Collective. All Rights Reversed.
-     </div>
+										<tr>
+                			<th><span class='label'>Is register_globals turned off?</span></th>
+                			<td><?php echo $ErrorMark['register_globals']; ?></td>
+										</tr>
+      
+										<tr>
+                			<th><span class='label'>Is the _storage/ directory writable?</span></th>
+                			<td><?php echo $ErrorMark['storage_directory']; ?></td>
+										</tr>
+      
+										<tr>
+                			<th><span class='label'>Is the configurations/ directory writable?</span></th>
+                			<td><?php echo $ErrorMark['configurations_directory']; ?></td>
+										</tr>
+      
+										<tr>
+                			<th><span class='label'>Is .htaccess writable?</span></th>
+                			<td><?php echo $ErrorMark['htaccess_final']; ?></td>
+										</tr>
+      
+										<tr>
+                			<th><span class='label'>PHP version 5.2 or higher? (Running <?php echo phpversion(); ?>)</span></th>
+                			<td><?php echo $ErrorMark['php_version']; ?></td>
+										</tr>
+      
+										<tr>
+                			<th><span class='label'>Mysql version 5.0 or higher? (Client is <?php echo $this->GetMysqlClientVersion (); ?>)</span></th>
+                			<td><?php echo $ErrorMark['mysql_client_version']; ?></td>
+										</tr>
+
+									</tbody>
+								</table>
+      
+              </fieldset>
+
+      				<fieldset>
+        				<legend>Completing Installation</legend>
+        				<p class='information'>Creating database tables and initializing Appleseed.</p>
+        
+								<table>
+									<tbody>
+										<tr>
+        							<th><span class='label'>Writing Configuration to File</span></th>
+        							<td><span class='done'>DONE</span></td>
+										</tr>
+        
+										<tr>
+        							<th><span class='label'>Creating .htaccess File</span></th>
+        							<td><span class='done'>DONE</span></td>
+										</tr>
+        
+										<tr>
+        							<th><span class='label'>Creating Appleseed Tables</span></th>
+        							<td><span class='done'>DONE</span></td>
+										</tr>
+        
+										<tr>
+        							<th><span class='label'>Importing Initial Data</span></th>
+        							<td><span class='done'>DONE</span></td>
+										</tr>
+        
+										<tr>
+        							<th><span class='label'>Creating Administrator Account</span></th>
+        							<td><span class='done'>DONE</span></td>
+										</tr>
+									</tbody>
+								</table>
+							</fieldset>
+        
+        			<p class='done'>
+        				Your Appleseed installation is now complete!  
+								For security reasons, be sure to change the 
+								permissions on <b>.htaccess</b> and 
+								<b>configurations/</b> so that they aren't 
+								web writable anymore, and delete the install
+        				script (index.php) from the appleseed root 
+								directory.
+							</p>
+        
+        			<h2 align="center"><a href='<?php echo $gDOMAIN; ?>/login/'>Click Here To Login To Your Appleseed Site</a></h2>
+            </form>
+	       </section>
+         </div>
+	     	 <div class="clear"></div>
+			 </div>
+
+       <footer class='container_12'>
+			  	<p align="center">
+        		Copyright &copy; 2004-2010 by Michael Chisari under the <a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU GPL Version 2</a>. All Rights Reserved.
+					</p>
+       </footer>
     </body>
     </html> <?php
     
