e4a69fe6e0fa2e904b1017917d9bb466032e13d9
Michael Chisari <michael.chisari@gmail.com>
2010-08-30 04:32:11 +0000

    Removed "debug" from layout foundations (since it's completed).

--- foundations/default/layout/comments.php
+++ foundations/default/layout/comments.php
@@ -435,42 +435,5 @@ It's a very good thing that the amount of anti-matter/anti-energy in the univers
     
 	<div class="clear"></div>
 	
-	<!-- Debug -->
-	<div id="appleseed-debug" class="container_16">
-		<div id="debug-menu" class="container_16">
-			<div id="debug-tabs" class="grid_16">
-				<nav>
-					<ul>
-						<li><a href="#">Warnings</a></li>
-						<li><a href="#">Errors</a></li>
-						<li><a href="#">SQL Queries</a></li>
-						<li class="selected" ><a href="#">Benchmarks</a></li>
-					</ul>
-				</nav>
-				
-			</div>
-		</div>
-		<div id="debug-container" class="container_16">
-			<div id="debug-main" class="grid_16">
-			
-				<section id="debug-warnings">
-					<h1>Warnings</h1>
-				</section>
-				
-				<section id="debug-errors">
-					<h1>Errors</h1>
-				</section>
-				
-				<section id="debug-queries">
-					<h1>SQL Queries</h1>
-				</section>
-				
-				<section id="debug-benchmarks">
-					<h1>Benchmarks</h1>
-				</section>
-			</div>
-		</div>
-	</div>
-    
 </body>
 </html>
--- foundations/default/layout/elements.php
+++ foundations/default/layout/elements.php
@@ -274,46 +274,5 @@
     
 	<div class="clear"></div>
 	
-	<!-- Debug -->
-	<div id="appleseed-debug" class="container_16">
-		<div id="debug-menu" class="container_16">
-			<div id="debug-tabs" class="grid_16">
-				<nav>
-					<ul>
-						<li class="ui-tabs-selected" ><a href="#debug-warnings">Warnings</a></li>
-						<li><a href="#debug-errors">Errors</a></li>
-						<li><a href="#debug-queries">SQL Queries</a></li>
-						<li><a href="#debug-benchmarks">Benchmarks</a></li>
-					</ul>
-				</nav>
-				
-			</div>
-		</div>
-		<div id="debug-container" class="container_16">
-			<div id="debug-main" class="grid_16">
-			
-				<section id="debug-warnings">
-					<h1>Warnings</h1>
-					<p>This week the insomnia is back. Insomnia, and now the whole world figures to stop by and take a dump on my grave. My boss is wearing his gray tie so today must be a Tuesday. My boss brings a sheet of paper to my desk and asks if I'm looking for something. This paper was left in the copy machine, he says, and begins to read: "The first rule of fight club is you don't talk about fight club." His eyes go side to side across the paper, and he giggles. "The second rule of fight club is you don't talk about fight club."</p>
-					
-				</section>
-				
-				<section id="debug-errors">
-					<h1>Errors</h1>
-				</section>
-				
-				<section id="debug-queries">
-					<h1>SQL Queries</h1>
-				</section>
-				
-				<section id="debug-benchmarks">
-					<h1>Benchmarks</h1>
-				</section>
-			</div>
-		</div>
-	</div>
-	
-	<div id="clear"></div>
-	
 </body>
 </html>
