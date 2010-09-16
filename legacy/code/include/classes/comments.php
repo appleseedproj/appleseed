87cda7c224b8a80f98cb07780df9e895b5f7fffd
Michael Chisari <michael.chisari@gmail.com>
2010-09-16 17:45:11 +0000

    Fixed syntax bug in commenting

--- legacy/code/include/classes/comments.php
+++ legacy/code/include/classes/comments.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting (E_ALL);
   // +-------------------------------------------------------------------+
   // | Appleseed Web Community Management Software                       |
   // | http://appleseed.sourceforge.net                                  |
