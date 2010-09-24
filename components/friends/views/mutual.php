a14363d88da8cca15e94f761bccb01549999807c
Michael Chisari <michael.chisari@gmail.com>
2010-09-24 06:25:34 +0000

    Added mutual view to friends component.
    Added selection of mutual tab in tabs controller of friends component.

---
+++ components/friends/views/mutual.php
@@ -0,0 +1,39 @@
+<section id="profile-friends">
+	<nav id="profile-friends-tabs" >
+	</nav>
+	
+	<span class="profile-friends-circle-add"><a href="#">Add A New Circle</a></span>
+	
+	<h1>
+		<span class='profile-friends-owner'></span>
+		<span class="profile-friends-circle-edit"><a href="#">Edit This Circle</a></span>
+		<span class="profile-friends-count additional-info"></span>
+		<span class="profile-friends-circle-remove"><a href="#">Remove This Circle</a></span>
+	</h1>
+	
+	<nav class='pagination'>
+	</nav>
+	
+	<ul class='friends-list'>
+		<li class='friends-list-item'>
+			<a href="" class="friends-icon-link"><img class="friends-icon" src=""></a>
+			<div class="friends-info">
+				<span class="friends-fullname"><a class="friends-fullname-link" href=""></a></span>
+				<span class="friends-circle-editor">
+					<select>
+						<option disabled="disabled">Add To Circle</option>
+						<option value="">&nbsp; Circle A</option>
+						<option disabled="disabled">Remove From Circle</option>
+						<option >&nbsp; Circle B</option>
+					</select>
+				</span>
+				<span class="friends-location">Location</span>
+				<p class="friends-status">Status</p>
+				<span class="friends-mutual-count">Mutual Count</span>
+				<span class="friends-add-friend"><a class="friends-add-friend-link" href="">Add as a friend</a></span>
+				<span class="friends-remove-friend"><a class="friends-remove-friend-link" href="">Remove friend</a></span>
+				<a href="" class="friends-identity">username@domain</a>
+			</div>
+		</li>
+	</ul>
+</section>
