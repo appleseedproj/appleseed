b3f78227b85e8bbf237323f07b83429fd2e2eff3
Michael Chisari <michael.chisari@gmail.com>
2010-10-19 13:05:03 +0800

    Added table create sql to update script

--- _release/update-0.7.8.sql
+++ _release/update-0.7.8.sql
@@ -1,1 +1,5 @@
-create table `#__PagePosts` ( `Post_PK` int(11) not null auto_increment, `User_FK` int(11) default null, `Action` char(32) default null, `ActionOwner` char(128) default null, `ActionLink` char(255) default null, `Description` text default null, `Comment` text default null, `Thumb` char default null, `Stamp` datetime default null, `Current` tinyint(1) default null, primary key (`Post_PK`) ) engine=myisam default charset=utf8;
+create table `#__PagePosts` ( `Post_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Owner` char(64) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Content` text, `Current` tinyint(1) DEFAULT NULL, PRIMARY KEY (`Post_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+create table `#__PageReferences` ( `Reference_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Identifier` char(32) DEFAULT NULL, `Type` char(16) DEFAULT NULL, `Stamp` datetime DEFAULT NULL, PRIMARY KEY (`Reference_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+create table `#__PrivacySettings` ( `Setting_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Circle_FK` int(11) DEFAULT NULL, `Type` char(32) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Everybody` tinyint(1) DEFAULT NULL, `Friends` tinyint(1) DEFAULT NULL, PRIMARY KEY (`Setting_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
