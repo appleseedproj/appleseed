17b53ba7aa28ca76a4fcc3c4366bf3280dcd65a4
Michael Chisari <michael.chisari@gmail.com>
2010-10-20 09:43:18 +0800

    Added private/protected/shared fields to the release sql file.

--- _release/update-0.7.8.sql
+++ _release/update-0.7.8.sql
@@ -3,3 +3,9 @@ create table `#__PagePosts` ( `Post_PK` int(11) NOT NULL AUTO_INCREMENT, `User_F
 create table `#__PageReferences` ( `Reference_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Identifier` char(32) DEFAULT NULL, `Type` char(16) DEFAULT NULL, `Stamp` datetime DEFAULT NULL, PRIMARY KEY (`Reference_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 create table `#__PrivacySettings` ( `Setting_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Circle_FK` int(11) DEFAULT NULL, `Type` char(32) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Everybody` tinyint(1) DEFAULT NULL, `Friends` tinyint(1) DEFAULT NULL, PRIMARY KEY (`Setting_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+alter table `#__friendCircles` add Private BOOL;
+
+alter table `#__friendCircles` add Private BOOL;
+
+alter table `#__friendCircles` add Private BOOL;
