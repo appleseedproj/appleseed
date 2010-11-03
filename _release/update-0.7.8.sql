a5236fff342d906111e64688c5cde0b1c9f66e20
Michael Chisari <michael.chisari@gmail.com>
2010-11-03 14:11:11 +0800

    Added NetworkCache table to db schema

--- _release/update-0.7.8.sql
+++ _release/update-0.7.8.sql
@@ -1,8 +1,8 @@
 create table `#__PagePosts` ( `Post_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Owner` char(64) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Content` text, `Current` tinyint(1) DEFAULT NULL, PRIMARY KEY (`Post_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-create table `#__PageReferences` ( `Reference_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Identifier` char(32) DEFAULT NULL, `Type` char(16) DEFAULT NULL, `Stamp` datetime DEFAULT NULL, PRIMARY KEY (`Reference_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+create table `#__PageReferences` ( `Reference_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Identifier` char(32) DEFAULT NULL, `Type` char(16) DEFAULT NULL, `Stamp` datetime DEFAULT NULL, PRIMARY KEY (`Reference_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-create table `#__PrivacySettings` ( `Setting_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Circle_FK` int(11) DEFAULT NULL, `Type` char(32) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Everybody` tinyint(1) DEFAULT NULL, `Friends` tinyint(1) DEFAULT NULL, PRIMARY KEY (`Setting_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+create table `#__PrivacySettings` ( `Setting_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) NOT NULL, `Circle_FK` int(11) DEFAULT NULL, `Type` char(32) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Everybody` tinyint(1) DEFAULT NULL, `Friends` tinyint(1) DEFAULT NULL, PRIMARY KEY (`Setting_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 alter table `#__friendCircles` add Private BOOL;
 
@@ -22,12 +22,14 @@ alter table `#__FriendInformation` drop `Alias`;
 
 alter table `#__FriendInformation` drop `sID`;
 
-create table `#__NotificationsOutgoing` ( `Outgoing_PK` int(11) NOT NULL AUTO_INCREMENT, `Recipient` char(128) DEFAULT NULL, `Owner_FK` int(11) NOT NULL, `ActionOwner` char(128) DEFAULT NULL, `Action` char(32) DEFAULT NULL, `ActionLink` char(255) DEFAULT NULL, `SubjectOwner` char(128) DEFAULT NULL, `ContextOwner` char(128) DEFAULT NULL, `Context` char(32) DEFAULT NULL, `ContextLink` char(255) DEFAULT NULL, `Icon` char(255) DEFAULT NULL, `Comment` text DEFAULT NULL, `Description` text, `Identifier` char(32) DEFAULT NULL, `Created` datetime DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Outgoing_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table `#__NotificationsOutgoing` ( `Outgoing_PK` int(11) NOT NULL AUTO_INCREMENT, `Recipient` char(128) DEFAULT NULL, `Owner_FK` int(11) NOT NULL, `ActionOwner` char(128) DEFAULT NULL, `Action` char(32) DEFAULT NULL, `ActionLink` char(255) DEFAULT NULL, `SubjectOwner` char(128) DEFAULT NULL, `ContextOwner` char(128) DEFAULT NULL, `Context` char(32) DEFAULT NULL, `ContextLink` char(255) DEFAULT NULL, `Icon` char(255) DEFAULT NULL, `Comment` text DEFAULT NULL, `Description` text, `Identifier` char(32) DEFAULT NULL, `Created` datetime DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Outgoing_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-create table `#__NotificationsIncoming` ( `Incoming_PK` int(11) NOT NULL AUTO_INCREMENT, `Owner_FK` int(11) NOT NULL, `ActionOwner` char(128) DEFAULT NULL, `Action` char(32) DEFAULT NULL, `ActionLink` char(255) DEFAULT NULL, `SubjectOwner` char(128) DEFAULT NULL, `ContextOwner` char(128) DEFAULT NULL, `Context` char(32) DEFAULT NULL, `ContextLink` char(255) DEFAULT NULL, `Icon` char(255) DEFAULT NULL, `Comment` text DEFAULT NULL, `Description` text, `Identifier` char(32) DEFAULT NULL, `Created` datetime DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Incoming_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table `#__NotificationsIncoming` ( `Incoming_PK` int(11) NOT NULL AUTO_INCREMENT, `Owner_FK` int(11) NOT NULL, `ActionOwner` char(128) DEFAULT NULL, `Action` char(32) DEFAULT NULL, `ActionLink` char(255) DEFAULT NULL, `SubjectOwner` char(128) DEFAULT NULL, `ContextOwner` char(128) DEFAULT NULL, `Context` char(32) DEFAULT NULL, `ContextLink` char(255) DEFAULT NULL, `Icon` char(255) DEFAULT NULL, `Comment` text DEFAULT NULL, `Description` text, `Identifier` char(32) DEFAULT NULL, `Created` datetime DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Incoming_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-create table `#__NotificationsUpdated` ( `Updated_PK` int(11) NOT NULL AUTO_INCREMENT, `Owner_FK` int(11) DEFAULT NULL, `Friend` char(128) DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Updated_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+create table `#__NotificationsUpdated` ( `Updated_PK` int(11) NOT NULL AUTO_INCREMENT, `Owner_FK` int(11) DEFAULT NULL, `Friend` char(128) DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Updated_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 create table `#__Janitor` ( `Updated` datetime DEFAULT NULL ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 insert into `#__Janitor` ( `Updated` ) values ( NOW() );
+
+create table `#__NetworkCache` ( `Request` varchar(512) DEFAULT NULL, `Response` text, `Created` datetime DEFAULT NULL ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
