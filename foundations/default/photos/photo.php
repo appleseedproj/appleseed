173575f38616e09dfe107e5b9a2f4d78471c4fc4
Michael Chisari <michael.chisari@gmail.com>
2010-11-08 22:38:21 +0800

    Added photos foundation info and definitions.

--- foundations/default/photos/photo.php
+++ foundations/default/photos/photo.php
@@ -1,68 +1,91 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    
-    <!-- Meta -->
-    <meta charset="utf-8" />
+	
+	<!-- Meta -->
+	<meta charset="utf-8" />
 
-    <!-- Title -->
-    <title><?php echo __("Photos For | Appleseed"); ?></title>
-    
-    <!-- Links -->
+	<!-- Title -->
+	<title><?php echo __("Info"); ?></title>
+	
+	<!-- Links -->
     <?php $zApp->Theme->UseStyles (); ?>
-    
-    <!-- Javascript --> 
-    <!--[if IE]>
-    <script src="js/html5.js"></script>
-    <![endif]-->
-    
+	
+	<!-- Javascript --> 
+	<!--[if IE]>
+	<script src="/themes/default/style/html5.js"></script>
+	<![endif]-->
+	
+    <!-- Load JLoader framework -->
+   	<script type="text/javascript" src="/libraries/javascript/jloader.init.js"></script>
+   	<script type="text/javascript" src="/foundations/default/default.js"></script>
+   	
+   	<!-- Load JQuery -->
+   	<script type="text/javascript" src="/libraries/external/JQuery-1.4.2/jquery-1.4.2.min.js"></script>
+   	
+   	<!-- Load JQuery::UI -->
+   	<script type="text/javascript" src="/libraries/external/JQuery-1.4.2/plugins/jquery-ui-1.8.2.custom.min.js"></script>
+   	
+   	<!-- Load JQuery::Validation -->
+   	<script type="text/javascript" src="/libraries/external/JQuery-1.4.2/plugins/jquery.validate.js"></script>
+   	
+   	<!-- Load JQuery::Preload -->
+   	<script type="text/javascript" src="/libraries/external/JQuery-1.4.2/plugins/jquery.preload-min.js"></script>
+   	
 </head>
 
 <body id="www-website-com">
 
-	<?php $zApp->Components->Go ( "system" ); ?>
+  	<?php $zApp->Components->Go ( "system" ); ?>
+	
+	<div class="clear"></div>
 
-    <header class="container_12">
-    
-  		<?php $zApp->Components->Go ( "header" ); ?>
-
-    </header>
-
-    <div class="clear"></div>
-
-    <div id="page" class="container_12">
-    
-        <div id="page_left" class="grid_3">
-        
-		  	<?php $zApp->Components->Go ( "profile" ); ?>
-		  	
-        </div>
-        
-        <div id="page_right" class="grid_9">
-        
-		  	<?php $zApp->Components->Go ( "profile", "menu", "menu" ); ?>
-		  	
-			<?php $zApp->Components->Go ( "photos", "photos", "photo" ); ?>
-        
-        </div>
-        
-        <div class="clear"></div>
+	<!-- System Message -->
+	<header id="appleseed-header">
+ 		<?php $zApp->Components->Go ( "header" ); ?>
+ 	</header>
+ 	
+	<div id="appleseed-logo"></div>
+	
+	<div id="appleseed-container" class="container_16">
+	
+    	<div id="appleseed-profile" class="container_16">
+    		<div id="appleseed-profile-status" class="container_16">
+    			<div id="status-container" class="grid_12 push_4">
+					<?php $zApp->Components->Go ( "profile", "status", "status" ); ?>
+    			</div>
+    		</div>
+	       	<div id="appleseed-profile-menu" class="container_16">
+	       		<nav id="profile-tabs" class="grid_9 push_4">
+					<?php $zApp->Components->Go ( "profile", "tabs", "tabs" ); ?>
+		       	</nav>
+		       	<div id="profile-search" class="grid_3 push_4">
+					<?php $zApp->Components->Go ( "search", "search", "local" ); ?>
+				</div>
+			</div>
+       
+			<div id="appleseed-profile-main" class="grid_16">
+				<div id="profile-photos" class="grid_16">
+					<div id="photos-navigation">
+					  	<?php $zApp->Components->Go ( "photos", "navigation", "navigation" ); ?>
+					</div>
+				</div>
+				<div id="appleseed-profile-content" class="grid_12 omega">
+					<?php $zApp->Components->Go ( "photos", "sets", "sets" ); ?>
+				</div>
+			</div>
+		</div>
         
     </div>
 
-    <div class="clear"></div>
-
-    <footer class="container_12">
-    
-		<?php $zApp->Components->Go ( "footer" ); ?>
-        
-        <div class="clear"></div>
-        
-    </footer>
-
-	<?php $zApp->Components->Go ( "debug" ); ?>
-        
+	<div class="clear"></div>
+	
+    <footer id="appleseed-footer" class="container_16">
+ 		<?php $zApp->Components->Go ( "footer" ); ?>
+ 	</footer>
+ 	
+	<div class="clear"></div>
+	
 </body>
 </html>
