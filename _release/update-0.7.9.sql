3a0e562e6c2367a3bf12cbd41846b40953ab98a2
Michael Chisari <michael.chisari@gmail.com>
2010-11-13 09:55:13 +0800

    Styling for link posts, sql table info for PageLinks

---
+++ _release/update-0.7.9.sql
@@ -0,0 +1,1 @@
+create table `#__PageLinks` ( `Link_PK` int(11) NOT NULL AUTO_INCREMENT, `User_FK` int(11) DEFAULT NULL, `Owner` char(64) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Content` text, `Link` char(255) DEFAULT NULL, `Title` char(64) DEFAULT NULL, `Description` text, `Thumb` char(255) DEFAULT NULL, PRIMARY KEY (`Link_PK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
