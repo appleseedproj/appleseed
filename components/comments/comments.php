e125bf0770ac61c4a138877cfbbbcc1ecb33248d
Michael Chisari <michael.chisari@gmail.com>
2010-11-19 02:41:21 +0800

    Added comments interface class file.

---
+++ components/comments/comments.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @version      $Id$
+ * @package      Appleseed.Components
+ * @subpackage   Comments
+ * @copyright    Copyright (C) 2004 - 2010 Michael Chisari. All rights reserved.
+ * @link         http://opensource.appleseedproject.org
+ * @license      GNU General Public License version 2.0 (See LICENSE.txt)
+ */
+
+// Restrict direct access
+defined( 'APPLESEED' ) or die( 'Direct Access Denied' );
+
+/** Comments Component
+ * 
+ * Comments Component Entry Class
+ * 
+ * @package     Appleseed.Components
+ * @subpackage  Comments
+ */
+class cComments extends cComponent {
+	
+	/**
+	 * Constructor
+	 *
+	 * @access  public
+	 */
+	public function __construct ( ) {       
+		parent::__construct();
+	}
+	
+}
+
