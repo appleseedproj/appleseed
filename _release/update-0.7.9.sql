38bdddd575c78dda860bc544d075868c740676cb
Michael Chisari <michael.chisari@gmail.com>
2010-11-27 08:36:03 +0800

    Added table definitions for journal component data.

--- _release/update-0.7.9.sql
+++ _release/update-0.7.9.sql
@@ -2,3 +2,8 @@ create table `#__PageLinks` ( `Link_PK` int(11) NOT NULL AUTO_INCREMENT, `User_F
 
 alter table `#__NotificationsOutgoing` add Title char(255);
 alter table `#__NotificationsIncoming` add Title char(255);
+
+drop table `#__journalPrivacy`;
+drop table `#__journalPost`;
+
+create table `#__JournalEntries` ( `Entry_FK` int(11) NOT NULL AUTO_INCREMENT, `Owner_FK` int(11) DEFAULT NULL, `Title` char(200) DEFAULT NULL, `Body` text, `Identifier` char(32) DEFAULT NULL, `Created` datetime DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Entry_FK`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
