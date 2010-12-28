bc7a810ae14b04f570305810f41a4f2164e56f3d
Michael Chisari <michael.chisari@gmail.com>
2010-12-28 13:36:25 +0800

    Prep view for search input

--- components/search/controllers/search.php
+++ components/search/controllers/search.php
@@ -51,6 +51,7 @@ class cSearchSearchController extends cController {
 			$this->Ask->Find ( '[name=search]', 0 )->disabled = true;
 		} else {
 			$this->Ask->Find ( '#search-global', 0 )->action = 'http://' . $this->_Current->Domain . '/search/';
+			$this->Ask->Synchronize();
 		}
 		
 		$this->Ask->Display();
--- components/search/views/global.php
+++ components/search/views/global.php
@@ -1,3 +1,3 @@
 <form id="search-global">
-<input type="text" name="keywords" placeholder="Search Placeholder Text" class="keywords"><input type="submit" value="" class="search-submit">
+<input type="text" name="k" placeholder="Search Placeholder Text" class="keywords"><input type="submit" value="" class="search-submit">
 </form>
