0755a30095ef0c47d0f53e0bd7bd18f2be6c06fe
Michael Chisari <mchisari@PICNets-MacBook-Pro.local>
2011-03-07 16:45:04 -0600

    Removed old photos tables, added new PhotoSets table

--- _release/update-0.7.10.sql
+++ _release/update-0.7.10.sql
@@ -9,7 +9,11 @@ insert into `#__Janitor` ( `Updated`, `Task` ) values ( NOW(), 'UpdateNodeNetwor
 
 insert into `#__Janitor` ( `Updated`, `Task` ) values ( NOW(), 'ProcessNewsfeed' );
 
-alter table `#__PhotoSets` change `Directory` `Directory` char(128) not null;
+drop table `asd_photoInformation`;
+drop table `asd_photoPrivacy`;
+drop table `asd_photoSets`;
+
+create table `asd_PhotoSets` ( `Set_PK` int(11) NOT NULL AUTO_INCREMENT, `Owner_FK` int(11) DEFAULT NULL, `Identifier` char(32) DEFAULT NULL, `Name` char(128) DEFAULT NULL, `Directory` char(128) NOT NULL, `Description` text, `Created` datetime DEFAULT NULL, `Updated` datetime DEFAULT NULL, PRIMARY KEY (`Set_PK`)) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
 alter table `#__NetworkNodes` drop `Methods`;
 
