a58b1ce07b46239ff0f400e3226160beec19c5f2
Michael Chisari <michael.chisari@gmail.com>
2011-03-07 02:46:49 -0600

    Added Entry field of NetworkNodes table to update sql

--- _release/update-0.7.10.sql
+++ _release/update-0.7.10.sql
@@ -14,3 +14,5 @@ alter table `#__PhotoSets` change `Directory` `Directory` char(128) not null;
 alter table `#__NetworkNodes` drop `Methods`;
 
 alter table `#__NetworkNodes` change `Version` `Version` char(32);
+
+alter table `#__NetworkNodes` add `Entry` varchar(128);
