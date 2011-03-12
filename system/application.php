db86485cf493ce9d16d7bf969274451238309571
Michael Chisari <mchisari@picnetmbp.(none)>
2011-03-12 00:41:40 -0600

    Added xml_encode function

--- system/application.php
+++ system/application.php
@@ -456,6 +456,48 @@ function rmkdir ( $path, $mode = 0777 ) {
 }
 
 /*
+ * Adapted from: http://darklaunch.com/2009/05/23/php-xml-encode-using-domdocument-convert-array-to-xml-json-encode
+ *
+ */
+function xml_encode($mixed,$domElement=null,$DOMDocument=null){
+    if(is_null($DOMDocument)){
+        $DOMDocument=new DOMDocument;
+        $DOMDocument->formatOutput=true;
+        xml_encode($mixed,$DOMDocument,$DOMDocument);
+        echo $DOMDocument->saveXML();
+    }
+    else{
+        if(is_array($mixed)){
+            foreach($mixed as $index=>$mixedElement){
+                if(is_int($index)){
+                    if($index==0){
+                        $node=$domElement;
+                    }
+                    else{
+                        $node=$DOMDocument->createElement($domElement->tagName);
+                        $domElement->parentNode->appendChild($node);
+                    }
+                }
+                else{
+                    $plural=$DOMDocument->createElement($index);
+                    $domElement->appendChild($plural);
+                    $node=$plural;
+                    if(rtrim($index,'s')!==$index){
+                        $singular=$DOMDocument->createElement(rtrim($index,'s'));
+                        $plural->appendChild($singular);
+                        $node=$singular;
+                    }
+                }
+                xml_encode($mixedElement,$node,$DOMDocument);
+            }
+        }
+        else{
+            $domElement->appendChild($DOMDocument->createTextNode($mixed));
+        }
+    }
+}
+
+/*
  * Factory class
  *
  */
