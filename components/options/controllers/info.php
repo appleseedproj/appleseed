572d5dcc1554a3a1be9ae0c5c4522985ed59dc68
Michael Chisari <michael.chisari@gmail.com>
2011-03-18 14:04:45 -0700

    Added beginnings of options component.

--- components/options/controllers/info.php
+++ components/options/controllers/info.php
@@ -30,7 +30,44 @@ class cOptionsInfoController extends cController {
 	}
 	
 	function Display ( $pView = null, $pData = array ( ) ) {
+
+		$Questions = $this->_Questions();
+		foreach ( $Questions as $g => $Group ) {
+			echo $Group->Label, "<br />";
+			foreach ( $Group->Questions as $q => $Question) {
+				echo '&nbsp; ' . $Question->Label, "<br />";
+			}
+		}
+
+		return ( true );
 		return ( parent::Display( $pView, $pData ) );
 	}
 
+	/*
+	 * Combine question configuration into array of class objects.
+	 *
+	 */
+	private function _Questions ( ) {
+		$Config = $this->Get ( 'Config' );
+		$quests = $Config['questions'];
+		$groups = $Config['question_groups'];
+		foreach ( $groups as $g => $group ) {
+			$Groups[$group] = new stdClass();
+			$Groups[$group]->Label = $Config['question_group_labels'][$g];
+			foreach ( $quests as $q => $quest ) {
+				if ( $Config['question_group'][$q] != $g ) continue;
+				$Groups[$group]->Questions[$quest] = new stdClass();
+				$Groups[$group]->Questions[$quest]->Label = $Config['question_labels'][$q];
+				$Groups[$group]->Questions[$quest]->Type = $Config['question_types'][$q];
+				$Groups[$group]->Questions[$quest]->Field = $Config['question_fields'][$q];
+				$Groups[$group]->Questions[$quest]->Social = $Config['question_social'][$q];
+				$Groups[$group]->Questions[$quest]->Display = $Config['question_display'][$q];
+				if ( $Config['question_options'][$q] )
+					$Groups[$group]->Questions[$quest]->Options = explode ( '|', $Config['question_options'][$q] );
+			}
+			ksort ( $Groups[$group]->Questions );
+		}
+		ksort ( $Groups );
+		return ( $Groups );
+	}
 }
